2cc4ee5f7f0dcd63f325fc360aa24e2d71109e08
pandaachuidc <53323186+pandaachuidc@users.noreply.github.com>
2020-02-27 16:32:46 +0800

    ADD FILES

--- js/common.js
+++ js/common.js
@@ -3,9 +3,9 @@
 $(window).scroll(function() {
   $(".animate").each(function() {
     if ($(this).isOnScreen(0, 0.1) == true) {
-      $(this).addClass("active");
+      $(this).addClass("on");
     } else {
-      $(this).removeClass("active");
+      $(this).removeClass("on");
     }
   });
 });
@@ -13,12 +13,12 @@ $(window).scroll(function() {
 //product details
 
 $(function() {
-  $(".items ").click(function() {
+  $(".point-item ,.info-item ").click(function() {
     var $this = $(this);
     if ($(this).hasClass("active")) {
       $(this).removeClass("active");
     } else {
-      $(".items").removeClass("active");
+      $(".point-item ,.info-item ").removeClass("active");
       $this.addClass("active");
     }
     return false;
@@ -44,11 +44,6 @@ $(".formfield.answer7").prepend('<span class="checkbox_span"></span>'),
   $(".formfield.name input").attr("placeholder", "請輸入姓名"),
   $(".formfield.mobile input").attr("placeholder", "請輸入手機號碼"),
   $(".formfield.formfield_email input").attr("placeholder", "請輸入電子信箱"),
-  $(".formfield.formfield_address1 input").attr("placeholder", "請輸入地址"),
-  $("#dwfrm_oto_oevent_answer4").attr(
-    "placeholder",
-    "請輸入Instagram帳號/粉絲人數"
-  ),
   $(".formfield.answer7 [for=dwfrm_oto_oevent_answer7]").html(
     '提醒您送出資料後，即代表您同意將此資料提供予Lancôme 蘭蔻進行行銷處理與使用<a href="http://www.lancome.com.tw/customer-service-security-policy.html" target="_blank">[詳細個人資料權益說明]</a>'
   ),
